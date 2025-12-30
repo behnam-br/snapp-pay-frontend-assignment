aab0256ed790f6090243aaa079be1171bfe51f76
Behnam Rostami <b.rostami@techwich.ac>
2025-12-30 13:35:15 +0330

    style: adde arro back to back link

--- src/app/contact/contact-back-link.tsx
+++ src/app/contact/contact-back-link.tsx
@@ -1,11 +1,13 @@
 import { Button, Link as MuiLink } from '@mui/material';
 
+import { LeftArrowIcon } from '@/components/icons/left-arrow';
 import { Link } from '@/lib/router/link';
 
 export function ContactBackLink() {
   return (
     <MuiLink to='/' component={Link} sx={{ textDecoration: 'none', width: 'fit-content' }}>
       <Button variant='outlined' size='small'>
+        <LeftArrowIcon />
         Back to contacts
       </Button>
     </MuiLink>
